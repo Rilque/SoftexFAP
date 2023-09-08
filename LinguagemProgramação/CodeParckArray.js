163b0d9823e1195b886051a488f3084e5096ae95
Rilque Rapozo <rickboddy@hotmail.com>
2023-09-08 11:22:17 -0300

    Create CodeParckArray.js
    Exercício do JoyClasss referente ao 3º modulo primeiro Code Arrays

---
+++ LinguagemProgramação/CodeParckArray.js
@@ -0,0 +1,14 @@
+var nomes = ["Lívia", "Eloá", "Rilque", "Heloísa", "Nando", "Alene", "Ravi", "Rildo", "Edileuza", "Levi"];
+var idades = [27, 10, 37, 10, 30, 60, 11, 78, 71, 19];
+var coresFavoritas = ["Rosa", "Azul", "Preto", "Lílas", "Cinza", "Amarelo", "Branco", "Laranja", "Vermelho", "Verde"];
+
+console.log("Nomes: ", nomes);
+console.log("Idades: ", idades);
+console.log("Cores Favoritas: ", coresFavoritas);
+
+idades[2] = 38;
+coresFavoritas[4] = "Violeta";
+
+console.log("Nomes: ", nomes);
+console.log("Idades: ", idades);
+console.log("Cores Favoritas: ", coresFavoritas);
