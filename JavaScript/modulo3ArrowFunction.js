de0d0847b1347aabdd4a8f9223b449d05928b5c5
Rilque Rapozo <rickboddy@hotmail.com>
2023-09-23 15:10:50 -0300

    On branch main  Your branch and 'origin/main' have diverged,  Changes to be committed: 	new file:   JavaScript/modulo3ArrowFunction.js  Changes not staged for commit: 	modified:   SoftexBackNoite2023.2 (modified content)

---
+++ JavaScript/modulo3ArrowFunction.js
@@ -0,0 +1,18 @@
+function saudacao() {
+    console.log("Olá, seja bem-vindo!");
+  }
+  
+  function soma(a, b) {
+    return a + b;
+  }
+  
+  const subtracao = (a, b) => a - b;
+  
+  saudacao();
+  
+  const num1 = 10;
+  const num2 = 5;
+  
+  console.log("Soma:", soma(num1, num2));
+  console.log("Subtração:", subtracao(num1, num2));
+  
